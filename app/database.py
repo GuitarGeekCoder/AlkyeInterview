e6c82e9755567f63a1b2a0ffe225476ca75f0701
Pratik Maheshwari <pratikmaheshwari321@gmail.com>
2026-06-05 11:10:21 +0530

    push for change in user config

--- app/database.py
+++ app/database.py
@@ -12,4 +12,3 @@ def create_session_factory(database_url: str) -> sessionmaker:
     engine = create_engine(database_url, connect_args=connect_args, future=True)
     Base.metadata.create_all(bind=engine)
     return sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
-
